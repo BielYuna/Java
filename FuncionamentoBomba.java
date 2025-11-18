2e1f248ec2354153810e26d1667569598ac2b1cc
Gabriel <bielyuna@gmail.com>
2025-11-18 17:11:43 -0300

    Ciação da classe responsavel por usar a bomba, assim como a criação de um objeto na mesma

--- FuncionamentoBomba.java
+++ FuncionamentoBomba.java
@@ -1,19 +1,17 @@
 public class FuncionamentoBomba {
     private boolean status;
-    private double nivelAgua;
 
     public FuncionamentoBomba() {
         this.status = false;
-        this.nivelAgua = 0.0;
     }
 
-    public void ligarBomba() {
+    public void ligarBomba(int segundos) {
         if (!status) {
             status = true;
             System.out.println("Bomba ligada.");
-            for (int i = 5; i > 0; i--) {
+            for (segundos = 5; segundos > 0; segundos--) {
                 System.out.println("Bomba em funcionamento...");
-                System.out.println("Desligando em " + i + " segundos.");
+                System.out.println("Desligando em " + segundos + " segundos.");
             }
             status = false;
             System.out.println("Bomba desligada automaticamente!");
@@ -30,34 +28,11 @@ public class FuncionamentoBomba {
             System.out.println("A bomba já está desligada.");
         }
     }
-
-    public void adicionarAgua(double quantidade) {
-        if (quantidade > 0) {
-            nivelAgua += quantidade;
-            System.out.println("Adicionado " + quantidade + " litros de água. Nível atual: " + nivelAgua + " litros.");
-        } else {
-            System.out.println("Quantidade inválida para adicionar água.");
-        }
-    }
-
-    public void bombearAgua(double quantidade) {
+    public void getStatus() {
         if (status) {
-            if (quantidade > 0 && quantidade <= nivelAgua) {
-                nivelAgua -= quantidade;
-                System.out.println("Bombearam " + quantidade + " litros de água. Nível atual: " + nivelAgua + " litros.");
-            } else {
-                System.out.println("Quantidade inválida para bombear água.");
-            }
+            System.out.println("A bomba está ligada.");
         } else {
-            System.out.println("A bomba está desligada. Não é possível bombear água.");
+            System.out.println("A bomba está desligada.");
         }
-    }
-
-    public double getNivelAgua() {
-        return nivelAgua;
-    }
-
-    public boolean status() {
-        return status;
     }
 }
---
+++ UsaBomba.java
@@ -0,0 +1,9 @@
+public class UsaBomba {
+    public static void main(String[] args) {
+        FuncionamentoBomba bomba1 = new FuncionamentoBomba();
+
+        bomba1.ligarBomba(5);
+        bomba1.desligarBomba();
+        bomba1.getStatus();
+    }
+}
