42378321f0c57eb4d3ba2ab730d59845d18f2b8f
Gabriel <bielyuna@gmail.com>
2025-11-18 21:39:18 -0300

    Implementações

--- parte1/UsaBomba.java
+++ parte1/UsaBomba.java
@@ -1,9 +1,11 @@
 package parte1;
+import javax.swing.JOptionPane;
 public class UsaBomba {
     public static void main(String[] args) {
         FuncionamentoBomba bomba1 = new FuncionamentoBomba();
+        int tempo = Integer.parseInt(JOptionPane.showInputDialog("Digite o tempo em segundos para a bomba ficar ligada:"));
 
-        bomba1.ligarBomba(6);
+        bomba1.ligarBomba(tempo);
         bomba1.desligarBomba();
         bomba1.getStatus();
     }
